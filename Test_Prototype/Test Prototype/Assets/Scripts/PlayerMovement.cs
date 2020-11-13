2710bc10ee641f6039c17e3abc72945cfa74561e
Chiurito <roc.belli@outlook.it>
2020-11-13 17:24:39 +0100

    CineMachine + First movement

---
+++ Test_Prototype/Test Prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+    private float moveInputH;
+    private float moveInputV;
+    private Rigidbody rigidB;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rigidB = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        moveInputH = Input.GetAxis("Horizontal");
+        moveInputV=  Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidB.velocity = new Vector3(moveInputH * moveSpeed, 0f, moveInputV * moveSpeed);
+    }
+}
