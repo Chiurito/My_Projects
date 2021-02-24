fe99a3198fb46ffbc51c3937fb8547ee12a5da6b
Chiurito <roc.belli@outlook.it>
2021-02-24 15:50:28 +0100

    bullet collisions and BulletDataSO

---
+++ 2D_Shooter/Assets/Scripts/DataSO/BulletDataSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapons/BulletData")]
+public class BulletDataSO : ScriptableObject
+{
+    
+}
