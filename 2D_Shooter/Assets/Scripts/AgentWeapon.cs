bfca8f783784e259714d52e3f2893f14280014ca
Chiurito <roc.belli@outlook.it>
2021-02-24 09:39:53 +0100

    Using AgentWeapon script to shoot

--- 2D_Shooter/Assets/Scripts/AgentWeapon.cs
+++ 2D_Shooter/Assets/Scripts/AgentWeapon.cs
@@ -10,9 +10,18 @@ public class AgentWeapon : MonoBehaviour
     [SerializeField]
     protected WeaponRenderer weaponRenderer;
 
+    [SerializeField]
+    protected Weapon weapon;
+
     private void Awake()
     {
+        AssignWeapon();
+    }
+
+    private void AssignWeapon()
+    {
         weaponRenderer = GetComponentInChildren<WeaponRenderer>();
+        weapon = GetComponentInChildren<Weapon>();
     }
 
     public virtual void AimWeapon (Vector2 pointerPosition)
@@ -25,7 +34,26 @@ public class AgentWeapon : MonoBehaviour
 
     protected void AdjustWeaponRendering()
     {
-        weaponRenderer?.FlipSprite(desiredAngle > 90 || desiredAngle < - 90);
-        weaponRenderer?.RenderBehindHead(desiredAngle < 180 && desiredAngle > 0);
+        if (weaponRenderer != null)
+        {
+            weaponRenderer.FlipSprite(desiredAngle > 90 || desiredAngle < -90);
+            weaponRenderer.RenderBehindHead(desiredAngle < 180 && desiredAngle > 0);
+        }
+    }
+
+    public void Shoot()
+    {
+        if (weapon != null)
+        {
+            weapon.Shoot();
+        }
+    }
+
+    public void StopShooting()
+    {
+        if (weapon != null)
+        {
+            weapon.StopShooting();
+        }
     }
 }
