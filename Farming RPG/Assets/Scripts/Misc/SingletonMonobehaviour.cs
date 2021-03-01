f2706ada923241b70e21e50e805e4e18dabb994e
Chiurito <roc.belli@outlook.it>
2021-03-01 11:26:00 +0100

    Player Class and Singleton monobehaviour Class

--- Farming RPG/Assets/Scripts/Misc/SingletonMonobehaviour.cs
+++ Farming RPG/Assets/Scripts/Misc/SingletonMonobehaviour.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SingletonMonobehaviour : MonoBehaviour
+public abstract class SingletonMonobehaviour<T> : MonoBehaviour where T: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static T instance;
+
+    public static T Instance
     {
-        
+        get
+        {
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Farming RPG/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Questa classe eredita la classe SingletonMonobehaviour. 
+//La passo con il parametro di tipo <Player> e viene effettuato il codice all'interno di SingletonMonobehaviour.
+//Lo scopo di SingletonMonobehaviour è controllare se ci sono più istanze dello stesso oggetto in esecuzione.
+public class Player : SingletonMonobehaviour<Player>
+{
+    
+}
