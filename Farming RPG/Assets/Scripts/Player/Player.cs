a7130ab00dec5c1d6a0e0034502fa3b09e8190ea
Chiurito <roc.belli@outlook.it>
2021-03-01 17:21:53 +0100

    .

--- Farming RPG/Assets/Scripts/Player/Player.cs
+++ Farming RPG/Assets/Scripts/Player/Player.cs
@@ -7,6 +7,69 @@
 public class Player : SingletonMonobehaviour<Player>
 {
     //Movement parameters
+    private float xInput;
+    private float yInput;
+    private bool isCarrying = false;
+    private bool isIdle;
+    private bool isLiftingToolDown;
+    private bool isLiftingToolLeft;
+    private bool isLiftingToolRight;
+    private bool isLiftingToolUp;
+    private bool isRunning;
+    private bool isUsingToolDown;
+    private bool isUsingToolLeft;
+    private bool isUsingToolRight;
+    private bool isUsingToolUp;
+    private bool isSwingingToolDown;
+    private bool isSwingingToolLeft;
+    private bool isSwingingToolRight;
+    private bool isSwingingToolUp;
+    private bool isWalking;
+    private bool isPickingUp;
+    private bool isPickingDown;
+    private bool isPickingLeft;
+    private bool isPickingRight;
+    private ToolEffect toolEffect = ToolEffect.none;
 
+    private Rigidbody2D rigidBody2D;
 
+    private Direction playerDirection;
+
+    private float movementSpeed;
+
+    private bool playerInputIsDisabled = false;
+
+    public bool PlalayerInputIsDisabled { get => playerInputIsDisabled; set => playerInputIsDisabled = value; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        #region Player Input
+
+        ResetAnimationTriggers();
+
+        #endregion
+    }
+
+    private void ResetAnimationTriggers()
+    {
+        isPickingRight = false;
+        isPickingLeft = false;
+        isPickingUp = false;
+        isPickingDown = false;
+        isUsingToolRight = false;
+        isUsingToolLeft = false;
+        isUsingToolUp = false;
+        isUsingToolDown = false;
+        isLiftingToolRight = false;
+        isLiftingToolLeft = false;
+        isLiftingToolUp = false;
+        isLiftingToolDown = false;
+    }
 }
