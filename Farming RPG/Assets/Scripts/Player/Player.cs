edc0b40f249be7d4a768fca0935fe47ee82333eb
Chiurito <roc.belli@outlook.it>
2021-03-02 09:47:34 +0100

    Basic player movement

--- Farming RPG/Assets/Scripts/Player/Player.cs
+++ Farming RPG/Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 
 //Questa classe eredita la classe SingletonMonobehaviour. 
@@ -54,7 +55,34 @@ public class Player : SingletonMonobehaviour<Player>
 
         ResetAnimationTriggers();
 
+        //Comportamento di default del player: prende la velocità del runningSpeed;
+        PlayerMovementInput();
+
+        //Comportamento secondario del player: prende la velocità del walkingSpeed;
+        PlayerWalkInput();
+
+        //Invia l'evento ad ogni subscriver(listener) per l'animazione
+        EventHandler.CallMovementEvent(xInput, yInput, isWalking, isRunning, isIdle, isCarrying,
+            toolEffect,
+            isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+            isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+            isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+            isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+            false, false, false, false);
+        
         #endregion
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        Vector2 move = new Vector2(xInput * movementSpeed * Time.deltaTime, yInput * movementSpeed * Time.deltaTime);
+
+        rigidBody2D.MovePosition(rigidBody2D.position + move);
     }
 
     private void ResetAnimationTriggers()
@@ -71,5 +99,73 @@ public class Player : SingletonMonobehaviour<Player>
         isLiftingToolLeft = false;
         isLiftingToolUp = false;
         isLiftingToolDown = false;
+        isSwingingToolRight = false;
+        isSwingingToolLeft = false;
+        isSwingingToolUp = false;
+        isSwingingToolDown = false;
+        toolEffect = ToolEffect.none;
+    }
+
+    private void PlayerMovementInput()
+    {
+        yInput = Input.GetAxisRaw("Vertical");
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        //Se il player si muove in diagonale normalizza il vettore
+        if (yInput != 0 && xInput != 0)
+        {
+            xInput = xInput * 0.71f;
+            yInput = yInput * 0.71f;
+        }
+
+        if (xInput != 0 || yInput != 0)
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+
+            //Capture player direction for savegame
+            if (xInput < 0)
+            {
+                playerDirection = Direction.left;
+            }
+            else if (xInput > 0)
+            {
+                playerDirection = Direction.right;
+            }
+            else if (yInput < 0)
+            {
+                playerDirection = Direction.down;
+            }
+            else
+            {
+                playerDirection = Direction.up;
+            }
+        }
+        else if(xInput == 0 && yInput == 0)
+        {
+            isRunning = false;
+            isWalking = false;
+            isIdle = true;
+        }
+    }
+
+    private void PlayerWalkInput()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            isRunning = false;
+            isWalking = true;
+            isIdle = false;
+            movementSpeed = Settings.walkingSpeed;
+        }
+        else
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+        }
     }
 }
