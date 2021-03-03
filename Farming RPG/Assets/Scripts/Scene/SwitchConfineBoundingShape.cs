caaac502fe872d45e1e2526e8f410306f210bbce
Chiuri <roc.belli@outlook.it>
2021-03-03 13:37:25 +0100

    Cinemachine bounds and bounds collider script

---
+++ Farming RPG/Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+
+    /// <summary>
+    /// Switch the collider that cinemachine use to define the edges of the screen
+    /// </summary>
+    private void SwitchBoundingShape()
+    {
+        // Cerca il Game Object con il tag specificato e gli assegna il componente specificato
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        // since the confiner bounds have changed need to call this to clear the cache
+        cinemachineConfiner.InvalidatePathCache();
+    }
+
+
+}
