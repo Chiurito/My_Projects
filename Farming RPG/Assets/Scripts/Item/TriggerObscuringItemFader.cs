bb968faf30b4da8586e54abce543d4b469789b95
Chiurito <roc.belli@outlook.it>
2021-03-03 16:16:25 +0100

    Fader

--- Farming RPG/Assets/Scripts/Item/TriggerObscuringItemFader.cs
+++ Farming RPG/Assets/Scripts/Item/TriggerObscuringItemFader.cs
@@ -5,6 +5,9 @@ public class TriggerObscuringItemFader : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Get the Game Object we have collided with and then get all the Obscuring item Fader components
+        // on it and its children - and then trigger the Fade Out
+
         ObscuringItemFader[] obscuringItemFader = collision.gameObject.GetComponentsInChildren<ObscuringItemFader>();
 
         if (obscuringItemFader.Length > 0)
@@ -12,6 +15,22 @@ public class TriggerObscuringItemFader : MonoBehaviour
             for (int i = 0; i < obscuringItemFader.Length; i++)
             {
                 obscuringItemFader[i].FadeOut();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Get the Game Object we have collided with and then get all the Obscuring item Fader components
+        // on it and its children - and then trigger the Fade In
+
+        ObscuringItemFader[] obscuringItemFader = collision.gameObject.GetComponentsInChildren<ObscuringItemFader>();
+
+        if (obscuringItemFader.Length > 0)
+        {
+            for (int i = 0; i < obscuringItemFader.Length; i++)
+            {
+                obscuringItemFader[i].FadeIn();
             }
         }
     }
