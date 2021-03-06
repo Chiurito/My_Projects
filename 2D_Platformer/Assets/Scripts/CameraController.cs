286ebc68fd36cd41438b92af2d4e61ac58382f9c
Chiuri <roc.belli@outlook.it>
2021-03-06 11:20:29 +0100

    Camera

--- 2D_Platformer/Assets/Scripts/CameraController.cs
+++ 2D_Platformer/Assets/Scripts/CameraController.cs
@@ -6,13 +6,29 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
 
+    public Transform farBackground, middleBackground;
+
+    public float minHeight, maxHeight;
+
+    private float lastXPos;
+
     void Start()
     {
-        
+        lastXPos = transform.position.x;
     }
 
     void Update()
     {
-        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        // Move the camera
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        float clampedY = Mathf.Clamp(transform.position.y, minHeight, maxHeight);
+        transform.position = new Vector3(transform.position.x, clampedY, 0f);
+
+        // Move layers
+        float amountToMoveX = transform.position.x - lastXPos;
+        farBackground.position += new Vector3(amountToMoveX, 0f, 0f);
+        middleBackground.position += new Vector3(amountToMoveX * 0.5f, 0f, 0f);
+
+        lastXPos = transform.position.x;
     }
 }
