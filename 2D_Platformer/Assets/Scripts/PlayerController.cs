3f52a3a92552dcaa67c74f212d3f918e73e91d49
Chiuri <roc.belli@outlook.it>
2021-03-06 10:47:53 +0100

    Animation

--- 2D_Platformer/Assets/Scripts/PlayerController.cs
+++ 2D_Platformer/Assets/Scripts/PlayerController.cs
@@ -12,9 +12,15 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheckPoint;
     public LayerMask whatIsGround;
 
+    private bool canDoubleJump;
+
+    private Animator anim;
+    private SpriteRenderer theSR;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -23,12 +29,37 @@ public class PlayerController : MonoBehaviour
 
         isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, 0.2f, whatIsGround);
 
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
             if (isGrounded)
             {
                 theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
             }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
         }
+
+        if (theRB.velocity.x < 0)
+        {
+            theSR.flipX = true;
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            theSR.flipX = false;
+        }
+
+        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("isGrounded", isGrounded);
     }
 }
