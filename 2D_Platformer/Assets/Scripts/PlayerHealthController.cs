85c711d5446d129fe153e0d66b819b4a9989dcd2
Chiurito <roc.belli@outlook.it>
2021-03-08 11:47:25 +0100

    Invincibility counter

--- 2D_Platformer/Assets/Scripts/PlayerHealthController.cs
+++ 2D_Platformer/Assets/Scripts/PlayerHealthController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerHealthController : MonoBehaviour
 {
     public static PlayerHealthController instance;
+    private SpriteRenderer theSR;
     
     public int currentHealth, maxHealth;
+
+    public float invincibleLenght;
+    private float invincibleCounter;
 
     private void Awake()
     {
@@ -15,22 +19,43 @@ public class PlayerHealthController : MonoBehaviour
 
     void Start()
     {
+        theSR = GetComponent<SpriteRenderer>();
         currentHealth = maxHealth;
     }
 
 
     void Update()
     {
-        
+        if (invincibleCounter > 0)
+        {
+            invincibleCounter -= Time.deltaTime;
+
+            if (invincibleCounter <= 0)
+            {
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1f);
+            }
+        }
     }
 
     public void DealDamage()
     {
-        currentHealth--;
-
-        if (currentHealth <= 0)
+        if (invincibleCounter <= 0)
         {
-            gameObject.SetActive(false);
+            currentHealth--;
+
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                invincibleCounter = invincibleLenght;
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 0.5f);
+            }
+
+            UIController.instance.UpdateHealthDisplay();
         }
     }
 }
---
+++ 2D_Platformer/Assets/Scripts/UIController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    public Image hearth1, heart2, heart3;
+
+    public Sprite heartFull, heartEmpty, heartHalf;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealthDisplay()
+    {
+        switch (PlayerHealthController.instance.currentHealth)
+        {
+            case 6:
+                hearth1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartFull;
+                break;
+
+            case 5:
+                hearth1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartHalf;
+                break;
+
+            case 4:
+                hearth1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartEmpty;
+                break;
+
+            case 3:
+                hearth1.sprite = heartFull;
+                heart2.sprite = heartHalf;
+                heart3.sprite = heartEmpty;
+                break;
+
+            case 2:
+                hearth1.sprite = heartFull;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+
+            case 1:
+                hearth1.sprite = heartHalf;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+
+            case 0:
+                hearth1.sprite = heartEmpty;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+        }
+    }
+}
