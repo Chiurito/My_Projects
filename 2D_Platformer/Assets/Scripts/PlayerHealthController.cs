d6974ef4ebe3294a51e6afe42066090659b45bf1
Chiurito <roc.belli@outlook.it>
2021-03-08 10:54:33 +0100

    UI

---
+++ 2D_Platformer/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+    
+    public int currentHealth, maxHealth;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
